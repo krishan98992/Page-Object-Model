b50dfdf86da19175896dd26085a53c0b9a5cabc4
krishan98992 <jha.kumar.krishan@gmail.com>
2022-03-03 22:43:45 +0530

    addded base page

--- src/main/java/com/qa/pages/HomePage.java
+++ src/main/java/com/qa/pages/HomePage.java
@@ -1,5 +1,10 @@
 package com.qa.pages;
 
 public class HomePage {
+	
+	public void base()
+	{
+		System.out.println("base");
+	}
 
 }
--- src/test/java/com/qa/test/testing.java
+++ src/test/java/com/qa/test/testing.java
@@ -1,5 +1,14 @@
 package com.qa.test;
 
+import org.testng.annotations.Test;
+
 public class testing {
+	
+	
+	@Test
+	public void testloginpage()
+	{
+		System.out.println("login checked");
+	}
 
 }
